79a6dd2a2582a902ccbf1d5074271cfa3f63049d
Jatin Anand <31433847+Jatin345Anand@users.noreply.github.com>
2020-03-18 11:22:51 +0530

    Update Users.js

--- Regask_db/Users.js
+++ Regask_db/Users.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected!");
-    var sql = "CREATE TABLE Users(userId int NOT NULL AUTO_INCREMENT,emailId varchar(255) NOT NULL,FirstName varchar(255),LastName varchar(255), hashCode longtext, salt varchar(255) ,lastLogin Date NOT NULL,roleId int,	company varchar(255),country varchar(255),phonePrefix varchar(3),phoneNumber varchar(10), industry varchar(255),active BOOLEAN, PRIMARY KEY (userId))"
+    var sql =    "CREATE TABLE Users(userId int NOT NULL AUTO_INCREMENT,emailId varchar(255) NOT NULL,FirstName varchar(255),LastName varchar(255),hashCode LONGTEXT,salt MEDIUMTEXT,lastLogin Date NOT NULL,roleId int,	company varchar(255),country varchar(255),phonePrefix varchar(255),phoneNumber varchar(255), industry varchar(255),active BOOLEAN, PRIMARY KEY (userId))";
     con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("UsersTable created");
